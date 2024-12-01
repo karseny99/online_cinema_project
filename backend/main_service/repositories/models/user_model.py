206c01c35dcdd6db5a67ccf2231c14c473980912
karseny99 <55881140+karseny99@users.noreply.github.com>
2024-12-01 21:53:14 +0300

    Update user_model.py

--- backend/main_service/repositories/models/user_model.py
+++ backend/main_service/repositories/models/user_model.py
@@ -12,7 +12,7 @@ class User(Base):
     username = Column(String(50), unique=True, nullable=False)
     password = Column(String(256), nullable=False)
     role = Column(String(10), default='reader', nullable=False)
-    register_at = Column(DateTime, default=datetime.now())
+    registered_at = Column(DateTime, default=datetime.now())
 
     @classmethod
     def from_orm(cls, user_orm):
@@ -22,5 +22,5 @@ class User(Base):
             username = user_orm.username,
             password = user_orm.password,
             role = user_orm.role,
-            register_at = user_orm.register_at
+            registered_at = user_orm.register_at
         )
