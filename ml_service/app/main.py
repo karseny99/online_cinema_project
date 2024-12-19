81c89c3d8a3082c47340a39f38cbb30ef47a44f8
Daniel <arseny.karimov@gmail.com>
2024-12-19 10:42:48 +0300

    ml class architecture added

--- ml_service/app/main.py
+++ ml_service/app/main.py
@@ -4,4 +4,11 @@ import repository.movie_user_repo as repo
 if __name__ == "__main__":
     recommendation_system = ml_service.MLRecomendation()
 
+    '''
+        репо слой предназначен для работы с базой данных, 
+        а здесь вызывается основная логика с него
+
+        Эта функция должна быть из сервиса и возвращать, 
+        например, имя файла, куда было сохранено, и для сохранения вызывать репозитории
+    '''
     repo.run_pipeline(recommendation_system)
--- ml_service/app/service/ml.py
+++ ml_service/app/service/ml.py
@@ -4,6 +4,97 @@ from scipy.sparse import csr_matrix
 from sklearn.neighbors import NearestNeighbors
 from repository.movie_user_repo import get_ratings, get_movies
 
+
+class MLRecommendation:
+
+    # https://github.com/rcz7795/Movie-Recommendation-System/blob/main/Movie_Recommendation_System.ipynb
+    # Тут буквально есть всё на требуемом уровне
+
+    def __init__(self):
+        '''
+            Здесь инициализация бустинга (xgboost | catboost) решайте сами
+            вызывается load_model
+
+            Первое: проверка, можно ли вгрузить веса или как-то 
+            загрузить локально сохраненную модель. 
+            Реализовать вгрузку локальных данных без обработки датафрейма. 
+            
+            Если нельзя:
+            сюда нужно вынести загрузку этих 20 млн рейтингов
+            с вызовом обработки данных.
+            Можно либо загружать с нуля всё здесь, 
+            либо использовать метод fit и в него кидать батчи
+            (То есть в data будет, например, информация по 1000 человек)
+            Но, подозреваю, будет больно с обработкой данных, потому что 
+            data_processing работает на всем датасете вроде как
+        '''
+        pass
+
+    def prepare_data(self):
+        pass
+
+    def fit(self, data: list[your_type]):
+        '''
+            Функция нужна для дообучения
+            data - это список, в котором элемент - это информация об оценках пользователей,
+            как выглядит эта информация вы решаете сами 
+            (она должна еще и совпадать с тем, что получится после data_preparation)
+        '''
+        # for user in data:
+        #     model.fit(user)
+
+    def predict_for_user(self, user_id: int) -> list:
+        '''
+            Посмотреть можно ли сделать предикт только для одного юзера 
+            без подсчета всей матрицы
+            Возврат list[movie_id]
+        '''
+        pass
+
+    def predict_relevance_matrix(self):
+        '''
+            Подсчет всей матрицы
+        '''
+        pass
+
+    def save_model(self, path: str) -> None:
+        '''
+            Примерно так
+            from catboost import CatBoostClassifier
+
+            # Создание и обучение модели
+            model = CatBoostClassifier(iterations=100, depth=3, learning_rate=0.1, loss_function='Logloss')
+            model.fit(X_train, y_train)
+
+            # Сохранение модели
+            model.save_model('catboost_model.cbm')
+
+        '''
+        pass
+    
+    def save_model_mlflow(self):
+        '''
+            Сохранить модель в MLflow (пока не надо)
+        '''
+        weights = self.get_model_weights()
+        pass
+
+    def load_model(self, path: str) -> None:
+        '''
+            Загрузить модель 
+            Пример
+            from catboost import CatBoostClassifier
+
+            # Создание экземпляра модели
+            model = CatBoostClassifier()
+
+            # Загрузка модели
+            model.load_model('catboost_model.cbm')
+
+        '''
+        pass
+
+
 class MLRecomendation:
     def __init__(self):
         # Параметры для инициализации
